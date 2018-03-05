4e76fe8c11ec65d46c7fb92974fab14e74f4b87a
airupaza <airupaza@gmail.com>
2018-03-06 01:17:28 +0500

    Initial commit

---
+++ A/src/test.java
@@ -0,0 +1,11 @@
+
+
+public class test {
+    public static void main(String[] args){
+        String s="ABCD_95";
+        s=s.toLowerCase();
+        System.out.println(s);
+        char[] c=s.toCharArray();
+        if (c[0]=='a') System.out.print(c[0]);
+    }
+}
---
+++ untitled/src/testString.java
@@ -0,0 +1,8 @@
+public class testString {
+    public static void main(String[] args){
+        char c='0';
+        String k="1";
+        k+=c;
+        System.out.print(k);
+    }
+}
